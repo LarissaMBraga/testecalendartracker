e3e238bc94c6f2ad108b912f511ce5f41779a80f
LarissaMBraga <147963110+LarissaMBraga@users.noreply.github.com>
2024-07-21 03:22:37 -0300

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Button, Modal, Text, TouchableOpacity, TextInput, ScrollView, StyleSheet } from 'react-native';
 import DatePicker from '../components/DatePicker';
 import MyCalendar from '../components/Calendar';
 
@@ -8,6 +8,15 @@ const HomeScreen: React.FC = () => {
   const [periodDates, setPeriodDates] = useState<string[]>([]);
   const [fertileWindow, setFertileWindow] = useState<string[]>([]);
   const [ovulationDate, setOvulationDate] = useState<string | null>(null);
+
+  // States for modal data
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [sexualActivity, setSexualActivity] = useState<{protected: boolean, unprotected: boolean}>({protected: false, unprotected: false});
+  const [feelings, setFeelings] = useState<string[]>([]);
+  const [symptoms, setSymptoms] = useState<string[]>([]);
+  const [contraceptive, setContraceptive] = useState<{today: boolean, yesterday: boolean, none: boolean}>({today: false, yesterday: false, none: false});
+  const [notes, setNotes] = useState('');
 
   const handleDateChange = (date: Date) => {
     const formattedDate = date.toISOString().split('T')[0];
@@ -56,6 +65,54 @@ const HomeScreen: React.FC = () => {
     return `${year}-${month}-${day}`;
   };
 
+  const handleDayPress = (day: { dateString: string }) => {
+    setSelectedDate(day.dateString);
+    setModalVisible(true);
+  };
+
+  const handleSave = () => {
+    // Save the data (sexualActivity, feelings, symptoms, contraceptive, notes) for the selected date
+    // Implement your logic here
+    setModalVisible(false);
+  };
+
+  const toggleSexualActivity = (activity: 'protected' | 'unprotected') => {
+    setSexualActivity((prev) => ({
+      ...prev,
+      [activity]: !prev[activity],
+    }));
+  };
+
+  const toggleContraceptive = (option: 'today' | 'yesterday' | 'none') => {
+    setContraceptive((prev) => ({
+      ...prev,
+      [option]: !prev[option],
+    }));
+  };
+
+  const toggleFeeling = (feeling: string) => {
+    setFeelings((prev) => {
+      if (prev.includes(feeling)) {
+        return prev.filter((f) => f !== feeling);
+      } else {
+        return [...prev, feeling];
+      }
+    });
+  };
+
+  const toggleSymptom = (symptom: string) => {
+    setSymptoms((prev) => {
+      if (prev.includes(symptom)) {
+        return prev.filter((s) => s !== symptom);
+      } else {
+        return [...prev, symptom];
+      }
+    });
+  };
+
+  const emotions = ['normal', 'triste', 'feliz', 'emotiva', 'depressiva', 'exausta', 'estressada'];
+  const symptomOptions = ['acne', 'piriri', 'dor abdominal', 'dor de cabeça', 'dor nas costas', 'enxaqueca'];
+
   return (
     <View>
       <DatePicker onDateChange={handleDateChange} />
@@ -64,9 +121,171 @@ const HomeScreen: React.FC = () => {
         periodDates={periodDates}
         fertileWindow={fertileWindow}
         ovulationDate={ovulationDate}
+        onDayPress={handleDayPress}
       />
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <ScrollView>
+          <View style={styles.modalContent}>
+            <Text>Data: {selectedDate}</Text>
+            <Text>Atividade Sexual</Text>
+            <View style={styles.sexualActivityContainer}>
+              <TouchableOpacity
+                style={[
+                  styles.sexualActivityButton,
+                  sexualActivity.protected && styles.selectedButton
+                ]}
+                onPress={() => toggleSexualActivity('protected')}
+              >
+                <Text style={styles.buttonText}>Protegido</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.sexualActivityButton,
+                  sexualActivity.unprotected && styles.selectedButton
+                ]}
+                onPress={() => toggleSexualActivity('unprotected')}
+              >
+                <Text style={styles.buttonText}>Desprotegido</Text>
+              </TouchableOpacity>
+            </View>
+            <Text>Emoções</Text>
+            <ScrollView horizontal style={styles.emotionsContainer}>
+              {emotions.map((emotion) => (
+                <TouchableOpacity
+                  key={emotion}
+                  style={[
+                    styles.emotionButton,
+                    feelings.includes(emotion) && styles.selectedButton
+                  ]}
+                  onPress={() => toggleFeeling(emotion)}
+                >
+                  <Text style={styles.buttonText}>{emotion}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+            <Text>Sintomas</Text>
+            <ScrollView horizontal style={styles.emotionsContainer}>
+              {symptomOptions.map((symptom) => (
+                <TouchableOpacity
+                  key={symptom}
+                  style={[
+                    styles.emotionButton,
+                    symptoms.includes(symptom) && styles.selectedButton
+                  ]}
+                  onPress={() => toggleSymptom(symptom)}
+                >
+                  <Text style={styles.buttonText}>{symptom}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+            <Text>Anticoncepcional</Text>
+            <View style={styles.contraceptiveContainer}>
+              <TouchableOpacity
+                style={[
+                  styles.contraceptiveButton,
+                  contraceptive.today && styles.selectedButton
+                ]}
+                onPress={() => toggleContraceptive('today')}
+              >
+                <Text style={styles.buttonText}>Hoje</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.contraceptiveButton,
+                  contraceptive.yesterday && styles.selectedButton
+                ]}
+                onPress={() => toggleContraceptive('yesterday')}
+              >
+                <Text style={styles.buttonText}>Ontem</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.contraceptiveButton,
+                  contraceptive.none && styles.selectedButton
+                ]}
+                onPress={() => toggleContraceptive('none')}
+              >
+                <Text style={styles.buttonText}>Não tem</Text>
+              </TouchableOpacity>
+            </View>
+            <Text>Anotação</Text>
+            <TextInput
+              value={notes}
+              onChangeText={setNotes}
+              placeholder="..."
+              multiline
+              style={styles.input}
+            />
+            <View style={styles.buttonContainer}>
+              <Button title="Salvar" onPress={handleSave} />
+              <Button title="Cancelar" onPress={() => setModalVisible(false)} />
+            </View>
+          </View>
+        </ScrollView>
+      </Modal>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modalContent: {
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  sexualActivityContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+},
+sexualActivityButton: {
+  padding: 10,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#ccc',
+},
+selectedButton: {
+  backgroundColor: '#ccc',
+},
+buttonText: {
+  color: '#000',
+},
+emotionsContainer: {
+  flexDirection: 'row',
+  marginBottom: 10,
+},
+emotionButton: {
+  padding: 10,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#ccc',
+  marginHorizontal: 5,
+},
+contraceptiveContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  marginBottom: 10,
+},
+contraceptiveButton: {
+  padding: 10,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#ccc',
+},
+});
 
 export default HomeScreen;
